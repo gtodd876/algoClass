469310a3235b7d9e600d0ca1159d43584bfec1cb
Todd Matthews <gtmatthews@gmail.com>
2017-12-03 16:31:46 -0500

    Add my recurionIntro solutions

--- recursion/recursionIntro.js
+++ recursion/recursionIntro.js
@@ -1,10 +1,32 @@
 //1. Write a function that loops through the numbers n down to 0. If you haven't done so try using a while loop to do this.
+function loopNums(n) {
+  while (n >= 1) {
+    n -= 1;
+  }
+  return
+}
 
 //2. Next, try looping just like above except using recursion
+function loopNums(n) {
+  if (n < 1) return;
+  loopNums(n-1);
+}
 
 //3.Write a function 'exponent' that takes two arguments base, and expo, uses a while loop to return the exponenet value of the base.
+function exponent(base, expo) {
+  var result = 1;
+  while (expo > 0) {
+    result *= base;
+    expo--;
+  }
+  return result;
+}
 
 //4. Write a function 'RecursiveExponent' that takes two arguments base, and expo, recursively returns exponent value of the base.
+function exponent(base, expo) {
+  if (expo === 1) return base;
+  return base * exponent(base, expo - 1);
+}
 
 //5. Write a function 'recursiveMultiplier' that takes two arguments, 'arr and num', and multiplies each arr value into by num and returns an array of the values.
 
